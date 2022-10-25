1175c2ff13602650512923b4cee5a42176e71362
Roy Macdonald <macdonald.roy@gmail.com>
2022-10-24 19:42:59 -0700

    Updated ouster lib to version 0.5.2

---
+++ libs/ouster/include/ouster/impl/build.h
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace ouster {
+
+const char* const BUILD_HASH = "unknown";
+
+const char* const BUILD_TYPE = "release";
+
+const char* const BUILD_SYSTEM = "Darwin";
+
+const char* const SDK_VERSION =
+    "0.5.2";
+
+const char* const SDK_VERSION_FULL =
+    "0.5.2+unknown-release";
+
+}
